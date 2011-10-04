c6b627fa0b9f498cebe6e2d62074a721d17c0c42
jsntv200 <jsntv200@gmail.com>
2011-10-05 10:34:20 +1100

    Check that the page has the factory-config part

--- lib/page_factory_config/admin/pages_controller.rb
+++ lib/page_factory_config/admin/pages_controller.rb
@@ -8,20 +8,24 @@ module PageFactoryConfig
       base.class_eval do
         def new_with_factory_config
           if params[:page_id].present?
-            begin
-              if factory_config = YAML::load(Page.find(params[:page_id]).render_part('factory-config'))
-                ['parts', 'fields', 'remove_parts', 'remove_fields'].each do |key|
-                  if config = factory_config[key]
-                    config.each do |value|
-                      model_class.send(key.singularize, value.delete('name'), value)
+            page = Page.find(params[:page_id])
+  
+            if page.has_part?('factory-config')
+              begin
+                if factory_config = YAML::load(page.render_part('factory-config'))
+                  ['parts', 'fields', 'remove_parts', 'remove_fields'].each do |key|
+                    if config = factory_config[key]
+                      config.each do |value|
+                        model_class.send(key.singularize, value.delete('name'), value)
+                      end
                     end
                   end
-                end
 
-                model_class.layout(factory_config['layout'])
+                  model_class.layout(factory_config['layout'])
+                end
+              rescue
+                raise PageFactoryConfigError
               end
-            rescue
-              raise PageFactoryConfigError
             end
           end
 
